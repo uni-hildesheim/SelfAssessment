3bc280f408f027d411bac5b99302ac159a1a6493
Christopher N. Hesse <raymanfx@gmail.com>
2019-01-20 13:01:19 +0100

    server: controller: Restructure pin create flow
    Preparing for the upcoming unit test integration: chained
    mongoose queries (promises) are bad because a single 'await'
    only waits for the first one to resolve, apparently.
    This is an issue because in unit tests, queries *must* be
    carried out in a synchronous fashion, otherwise they will always
    fail because of pending promises.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- backend/app/controller/pincode.controller.js
+++ backend/app/controller/pincode.controller.js
@@ -5,41 +5,42 @@ module.exports = {
 }
 
 // create a pseudo-random pin code
-function create(req, res) {
+async function create(req, res) {
     // length of the pin code, where each element is a digit
     // 8 --> 10*10*10*10*10*10*10*10 = 100.000.000 possibilities
     const LENGTH = 8
     const MAX_PINCODES = Math.pow(10, LENGTH);
 
+    // get all current pincodes
+    try {
+        var pincodes = await db.Pincode.find();
+    } catch(err) {
+        res.status(500).json({ error: 'DB error' });
+        return;
+    }
+
+    if (pincodes.length === MAX_PINCODES) {
+        res.status(500).json({ error: 'Exhausted random pincode possibilities' });
+        return;
+    }
+
+    // generate a new pincode
     var digits = new Array(LENGTH)
     var string = ''
-
-    // get all current pincodes
-    db.Pincode.find().then(pincodes => {
-        if (pincodes.length === MAX_PINCODES) {
-            res.status(500).json({ error: 'Exhausted random pincode possibilities' });
-            return;
+    do {
+        string = ''
+        for (var i = 0; i < LENGTH; i++) {
+            digits[i] = Math.floor(Math.random() * 10)
+            string += digits[i]
         }
+    } while (pincodes.indexOf(Number.parseInt(string)) > -1);
 
-        // generate a new pincode
-        do {
-            string = ''
-            for (var i = 0; i < LENGTH; i++) {
-                digits[i] = Math.floor(Math.random() * 10)
-                string += digits[i]
-            }
-        } while (pincodes.indexOf(Number.parseInt(string)) > -1);
-
-        // add the new pincode to the db collection
-        db.Pincode.create({
-            pin: Number.parseInt(string),
-            created: new Date()
-        }).then(pincode => {
-            res.status(201).json(pincode);
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        });
+    // add the new pincode to the db collection
+    db.Pincode.create({
+        pin: Number.parseInt(string),
+        created: new Date()
+    }).then(pincode => {
+        res.status(201).json(pincode);
     }).catch(err => {
         console.log(err);
         res.status(500).json({ error: err });
