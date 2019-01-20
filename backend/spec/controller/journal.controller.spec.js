7055048f121ad88463698a691941e687d7b4ffc6
Christopher N. Hesse <raymanfx@gmail.com>
2019-01-20 14:15:25 +0100

    server: spec: Add journal controller spec
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

---
+++ backend/spec/controller/journal.controller.spec.js
@@ -0,0 +1,60 @@
+const sinon = require('sinon');
+
+const JournalController = require('../../app/controller/journal.controller');
+const JournalModel = require('../../app/model/journal.model');
+
+describe('JournalController', () => {
+    const docs = [
+        {
+            associatedPin: 12345678,
+            data: 'dummy'
+        }
+    ];
+
+    afterEach(function () {
+        // cleanup and remove stubs
+        sinon.restore();
+    });
+
+    describe('JournalController.load', () => {
+        it('should return HTTP 404 for invalid pins', async () => {
+            sinon.stub(JournalModel, 'findOne').resolves(null);
+            const req = {
+                body: {
+                    pin: 'dummy'
+                }
+            };
+            const res = {
+                status: sinon.stub().returns({
+                    json: sinon.spy()
+                })
+            };
+            await JournalController.load(req, res);
+            sinon.assert.calledOnce(JournalModel.findOne);
+            sinon.assert.calledOnce(res.status);
+            sinon.assert.calledWith(res.status, 404);
+            sinon.assert.calledOnce(res.status().json);
+            sinon.assert.calledWith(res.status().json, { error: 'No journal for pin: NaN' });
+        });
+
+        it('should load the dummy journal from the stub DB', async () => {
+            sinon.stub(JournalModel, 'findOne').resolves(docs[0]);
+            const req = {
+                body: {
+                    pin: docs[0].associatedPin
+                }
+            };
+            var res = {
+                status: sinon.stub().returns({
+                    json: sinon.spy()
+                })
+            };
+            await JournalController.load(req, res);
+            sinon.assert.calledOnce(JournalModel.findOne);
+            sinon.assert.calledOnce(res.status);
+            sinon.assert.calledWith(res.status, 200);
+            sinon.assert.calledOnce(res.status().json);
+            sinon.assert.calledWith(res.status().json, docs[0]);
+        });
+    });
+});
