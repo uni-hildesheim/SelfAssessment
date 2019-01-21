f1a44594499b34ca3f2f3d2a70207cca9a500b33
Jonathan <Jonathan2008@gmx.de>
2019-01-21 19:04:14 +0100

    app: shared module configuration

--- SelfAssessment/src/app/shared/shared.module.ts
+++ SelfAssessment/src/app/shared/shared.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PinDialogComponent } from './components/dialogs/pin-dialog/pin-dialog.component';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [],
+  entryComponents: [PinDialogComponent],
+  declarations: [PinDialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    PinDialogComponent
   ]
 })
 export class SharedModule { }
