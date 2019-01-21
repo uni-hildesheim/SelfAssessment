e7dd63991450f8f1b76b24b802bee021a8f2fc01
Jonathan <Jonathan2008@gmx.de>
2019-01-21 19:07:57 +0100

    app: Add EntryComponent

---
+++ SelfAssessment/src/app/core/components/entry/entry.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EntryComponent } from './entry.component';
+import { TestpanelModule } from 'src/app/testpanel/testpanel.module';
+import { HttpClientModule } from '@angular/common/http';
+
+xdescribe('EntryComponent', () => {
+  let component: EntryComponent;
+  let fixture: ComponentFixture<EntryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EntryComponent ],
+      imports: [TestpanelModule, HttpClientModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EntryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
