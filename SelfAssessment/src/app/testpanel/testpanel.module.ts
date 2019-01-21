fd52b49893af495e3f4dc7aa2e0d781d20ea29e3
Jonathan <Jonathan2008@gmx.de>
2019-01-21 18:50:40 +0100

    app: configuration for the testpanel

---
+++ SelfAssessment/src/app/testpanel/testpanel.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestpanelRoutingModule } from './testpanel-routing.module';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
+import { SingleTestCardComponent } from './components/single-test-card/single-test-card.component';
+import { MaterialModule } from '../material/material.module';
+import { GlobalIndicator } from './global.indicators';
+import { InfopageComponent } from './components/infopage/infopage.component';
+
+@NgModule({
+  declarations: [MainPanelComponent, SingleTestCardComponent, InfopageComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    TestpanelRoutingModule
+  ],
+  providers: [
+    GlobalIndicator
+  ],
+  exports: [
+    MainPanelComponent
+  ]
+})
+export class TestpanelModule { }
