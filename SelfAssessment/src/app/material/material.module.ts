a4cab76a563eb12d3ca5d3d08ba190a2d670a428
Jonathan <Jonathan2008@gmx.de>
2019-01-21 18:00:02 +0100

    app: existing material dependencies

--- SelfAssessment/src/app/material/material.module.ts
+++ SelfAssessment/src/app/material/material.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatStepperModule, MatCardModule, MatRadioModule, MatCheckboxModule,
+  MatIconModule, MatButtonModule, MatProgressBarModule, MatGridListModule,
+  MatListModule, MatToolbarModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatProgressSpinnerModule,
+} from '@angular/material';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatStepperModule,
+    MatCardModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    MatListModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule { }
