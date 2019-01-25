6a8fe97b9cd321f1368c295321b37cc31d164f7c
Christopher N. Hesse <raymanfx@gmail.com>
2019-01-25 18:39:52 +0100

    server: Refactor entry script
     * modularize
     * log environment overrides
     * factor out main() function
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,109 +1,142 @@
 // load environment config
 require('dotenv').config();
 
-const fs = require('fs');
-const logger = require('./app/utils/logger');
-
-// static configuration
-var PORT = 8000;
-
-const db = require('./app/mongodb/db.js');
-var DB_URI = db.config.uri;
-
-if (process.env.DB_URI) {
-    DB_URI = process.env.DB_URI;
-}
-
-if (process.env.DB_USER) {
-    db.config.options.user = process.env.DB_USER;
-}
-
-if (process.env.DB_PASS) {
-    db.config.options.pass = process.env.DB_PASS;
-}
-
-if (process.env.APP_LOGLEVEL) {
-    if (!(process.env.APP_LOGLEVEL in logger.Level)) {
-        logger.log(logger.Level.ERROR, 'Invalid log level specified: ' + process.env.APP_LOGLEVEL);
-    } else {
-        logger.setLogLevel(process.env.APP_LOGLEVEL);
-    }
-}
-
-if (process.env.APP_PORT) {
-    PORT = process.env.APP_PORT;
-}
-
-// load dependencies
-const express = require('express');
+// load 3rdparty dependencies
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const express = require('express');
+const fs = require('fs');
 
-// create the app
-const app = express();
-app.use(express.static('data/assets/public'));
-app.use(bodyParser.json())
+// load local dependencies
+const db = require('./app/mongodb/db.js');
+const logger = require('./app/utils/logger');
+const router = require('./app/routes/index.js');
 
-// enable cross-origin resource sharing
-const corsOptions = {
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// static configuration
+let APP_LISTEN_PORT = 8000;
+
+function loadEnvironment() {
+    const APP_LOGLEVEL = process.env.APP_LOGLEVEL;
+    const APP_PORT = process.env.APP_PORT;
+    const DB_URI = process.env.DB_URI;
+    const DB_USER = process.env.DB_USER;
+    const DB_PASS = process.env.DB_PASS;
+
+    if (DB_URI) {
+        logger.log(logger.Level.INFO, 'env: DB_URI=' + DB_URI);
+        db.config.uri = DB_URI;
+    }
+
+    if (DB_USER) {
+        logger.log(logger.Level.INFO, 'env: DB_USER=' + DB_USER);
+        db.config.options.user = DB_USER;
+    }
+
+    if (DB_PASS) {
+        logger.log(logger.Level.INFO, 'env: DB_PASS=' + DB_PASS);
+        db.config.options.pass = DB_PASS;
+    }
+
+    if (APP_LOGLEVEL) {
+        if (!(APP_LOGLEVEL in logger.Level)) {
+            logger.log(logger.Level.ERROR, 'Invalid log level specified: ' + APP_LOGLEVEL);
+        } else {
+            logger.log(logger.Level.INFO, 'env: APP_LOGLEVEL=' + APP_LOGLEVEL);
+            logger.setLogLevel(APP_LOGLEVEL);
+        }
+    }
+
+    if (APP_PORT) {
+        logger.log(logger.Level.INFO, 'env: APP_PORT=' + APP_PORT);
+        APP_LISTEN_PORT = APP_PORT;
+    }
 }
-app.use(cors(corsOptions));
 
-// load the API routes
-require('./app/routes/index.js')(app);
+function createApp() {
+    const app = express();
+    app.use(express.static('data/assets/public'));
+    app.use(bodyParser.json());
 
-// database initialization
-logger.log(logger.Level.INFO, 'MongoDB URI: ' + DB_URI);
-db.connect(DB_URI, db.config.options);
+    // enable cross-origin resource sharing
+    const corsOptions = {
+        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    };
+    app.use(cors(corsOptions));
 
-// drop all course configs from the db
-db.Course.deleteMany({
-    // wildcard filter
-}).then(res => { // eslint-disable-line no-unused-vars
-    // swallow
-}).catch(err => {
-    logger.log(logger.Level.WARN, 'Failed to drop course documents from db: ' + err);
-});
+    // load the API routes
+    router(app);
 
-// read available configs from local data dir
-// TODO: validate configs
-fs.readdir('./data/configs', (err, items) => {
-    if (err) {
-        logger.log(logger.Level.ERROR, err);
-        return;
-    }
+    return app;
+}
 
-    for (const item of items) {
-        if (!item.endsWith('.json')) {
-            // we only handle JSON files, so just exit in this case
-            continue;
-        }
+function loadCourses(path) {
+    // drop all current course configs from the db
+    db.Course.deleteMany({
+        // wildcard filter
+    }).then(res => { // eslint-disable-line no-unused-vars
+        // swallow
+    }).catch(err => {
+        logger.log(logger.Level.WARN, 'Failed to drop course documents from db: ' + err);
+    });
 
-        let courseConfig;
-        try {
-            courseConfig = JSON.parse(fs.readFileSync('./data/configs/' + item));
-        } catch (err) {
-            logger.log(logger.Level.WARN, 'Not a valid JSON file: ' + item + ': ' + err);
-            continue;
-        }
-        const courseName = courseConfig['title'];
-        if (!courseName) {
-            logger.log(logger.Level.WARN, 'Config file does not have a valid title: ' + item);
-            continue;
-        }
-
-        db.Course.create({
-            name: courseName,
-            config: courseConfig
-        }).then(course => {
-            logger.log(logger.Level.INFO, 'Created course in db: ' + course.name);
-        }).catch(err => {
+    // read available configs from local data dir
+    // TODO: validate configs
+    fs.readdir(path, (err, items) => {
+        if (err) {
             logger.log(logger.Level.ERROR, err);
-        });
-    }
-});
+            return;
+        }
 
-app.listen(PORT, () => {
-    logger.log(logger.Level.INFO, 'Server running on port: ' + PORT);
-});
+        for (const item of items) {
+            if (!item.endsWith('.json')) {
+                // we only handle JSON files, so just exit in this case
+                continue;
+            }
+
+            let courseConfig;
+            try {
+                courseConfig = JSON.parse(fs.readFileSync(path + '/' + item));
+            } catch (err) {
+                logger.log(logger.Level.WARN, 'Not a valid JSON file: ' + item + ': ' + err);
+                continue;
+            }
+            const courseName = courseConfig['title'];
+            if (!courseName) {
+                logger.log(logger.Level.WARN, 'Config file does not have a valid title: ' + item);
+                continue;
+            }
+
+            db.Course.create({
+                name: courseName,
+                config: courseConfig
+            }).then(course => {
+                logger.log(logger.Level.INFO, 'Created course in db: ' + course.name);
+            }).catch(err => {
+                logger.log(logger.Level.ERROR, err);
+            });
+        }
+    });
+}
+
+function main() {
+    loadEnvironment();
+
+    // create the app
+    const app = createApp();
+
+    // connect to DB
+    logger.log(logger.Level.INFO, 'MongoDB URI: ' + db.config.uri);
+    db.connect(db.config.uri, db.config.options);
+
+    // sync course configs
+    loadCourses('./data/configs');
+
+    // actually start the Express.js server
+    app.listen(APP_LISTEN_PORT, () => {
+        logger.log(logger.Level.INFO, 'Server running on port: ' + APP_LISTEN_PORT);
+    });
+}
+
+if (require.main == module) {
+    main();
+}
