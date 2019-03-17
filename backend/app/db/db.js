b6d8cf1cfa832b6c04827fe76ebfb99fffccc133
Christopher N. Hesse <raymanfx@gmail.com>
2019-03-17 15:41:43 +0100

    server: db: Improve connection handling
     * Track connection state and expose it
     * Print disconnection events only once
     * Return a Promise in connect(): this way, users of this module
       can choose to synchronously attempt to establish a first
       connection
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- backend/app/db/db.js
+++ backend/app/db/db.js
@@ -2,68 +2,89 @@ const config = require('./config');
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 
-//mongoose.connection.on('error', console.error.bind(
-//    console, 'MongoDB connection error: ')
-//);
+const ConnectionState = {
+    ERROR: 0,
+    DISCONNECTED: 1,
+    CONNECTED: 2
+}
+
+let GLOBAL_STATE = ConnectionState.DISCONNECTED;
+
 mongoose.connection.on('error', function (err) {
     if (err.message && err.message.match(/failed to connect to server .* on first connect/)) {
         // we handle this special case in connect() down below
         return;
     }
+    GLOBAL_STATE = ConnectionState.ERROR;
     logger.error('MongoDB error: ' + err);
 });
-mongoose.connection.on('connecting', function () {
-    logger.info('MongoDB connecting');
-});
 mongoose.connection.once('connected', function () {
+    GLOBAL_STATE = ConnectionState.CONNECTED;
     logger.info('MongoDB connected successfully!');
 });
 mongoose.connection.on('disconnected', function() {
-    logger.info('MongoDB disconnected');
+    if (GLOBAL_STATE !== ConnectionState.DISCONNECTED) {
+        GLOBAL_STATE = ConnectionState.DISCONNECTED;
+        logger.info('MongoDB disconnected');
+    }
 });
 mongoose.connection.on('reconnected', function () {
+    GLOBAL_STATE = ConnectionState.CONNECTED;
     logger.info('MongoDB reconnected');
-});
-mongoose.connection.on('reconnectFailed', function () {
-    logger.info('MongoDB reconnecting failed');
 });
 
 // https://github.com/Automattic/mongoose/issues/5169#issuecomment-314983113
 function connect(dbURL, options) {
     var connection = mongoose.connection;
+
     mongoose.connect(dbURL, options);
 
-    connection.on('error', function (err) {
-        // If first connect fails because mongod is down, try again later.
-        // This is only needed for first connect, not for runtime reconnects.
-        // See: https://github.com/Automattic/mongoose/issues/5169
-        if (err.message && err.message.match(/failed to connect to server .* on first connect/)) {
-            // Wait for a bit, then try to connect again
-            setTimeout(function () {
-                logger.info('MongoDB retrying first connect...');
-                connection.openUri(dbURL, options).catch(() => {});
-                // Why the empty catch?
-                // Well, errors thrown by db.open() will also be passed to .on('error'),
-                // so we can handle them there, no need to log anything in the catch here.
-                // But we still need this empty catch to avoid unhandled rejections.
-            }, 1000);
-        } else {
-            // Some other error occurred.  Log it.
-            logger.error('MongoDB error: ' + err);
-        }
-    });
+    return new Promise((resolve, reject) => {
+        const retryIntervalMS = 1000;
+        let retryTimeElapsedMS = 0;
 
-    connection.once('open', function () {
-        logger.info('MongoDB connection established');
-    });
+        connection.on('error', function (err) {
+            // If first connect fails because mongod is down, try again later.
+            // This is only needed for first connect, not for runtime reconnects.
+            // See: https://github.com/Automattic/mongoose/issues/5169
+            if (err.message && err.message.match(/failed to connect to server .* on first connect/)) {
+                // if there's no more retry time available, bail out
+                if (options.connectTimeoutMS > 0 && retryTimeElapsedMS > options.connectTimeoutMS) {
+                    reject(err);
+                    return;
+                }
 
-    return connection;
+                // Wait for a bit, then try to connect again
+                setTimeout(function () {
+                    logger.info('MongoDB retrying first connect...');
+                    connection.openUri(dbURL, options).catch(() => {});
+                    // Why the empty catch?
+                    // Well, errors thrown by db.open() will also be passed to .on('error'),
+                    // so we can handle them there, no need to log anything in the catch here.
+                    // But we still need this empty catch to avoid unhandled rejections.
+
+                    retryTimeElapsedMS += retryIntervalMS;
+                }, retryIntervalMS);
+            } else {
+                // Some other error occurred.
+                reject(err);
+                return;
+            }
+        });
+    
+        connection.once('connected', function () {
+            resolve(connection);
+        });
+    });
 }
 
 module.exports = {
     // generic connection data
     config: config,
     connect: connect,
+
+    // global connection state
+    state: GLOBAL_STATE,
 
     // models
     Course: require('../core/course/course.model'),
