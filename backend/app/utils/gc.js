373422f70ca3bfe5ff08272eb138f42fadb8f07e
Christopher N. Hesse <raymanfx@gmail.com>
2019-03-19 13:23:24 +0100

    server: utils: gc: Extend API
    Provide removeTask() and reset() APIs.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- backend/app/utils/gc.js
+++ backend/app/utils/gc.js
@@ -11,6 +11,7 @@ let SETTINGS = {
  * Add a new GC task to be executed when the GC engine is triggered.
  * @param {function} fn Function to run.
  * @param {*} params Function parameters.
+ * @returns true on success, false if fn is not a function object
  */
 function addTask(fn, params) {
     if (typeof fn !== 'function') {
@@ -21,34 +22,61 @@ function addTask(fn, params) {
         fn: fn,
         params: params
     });
+
+    return true;
+}
+
+/**
+ * Remove a function from the internal task list.
+ * @param {function} fn Function to run.
+ * @returns true on success, false if fn was not found
+ */
+function removeTask(fn) {
+    let index = -1;
+
+    for (let i = 0; i < TASKS.length; i++) {
+        // must be the same memory location, thus '==='
+        if (TASKS[i].fn === fn) {
+            index = i;
+        }
+    }
+
+    if (index === -1) {
+        return false;
+    }
+
+    // remove from task list
+    TASKS.splice(index, 1);
+
+    return true;
 }
 
 /**
  * Pause the GC engine.
- * @param {Number} delay Amount of seconds to wait before starting the GC engine again.
+ * @param {Number} delay Milliseconds to wait before starting the GC engine again.
  */
 function pause(delay) {
     stop();
     setTimeout( () => {
         start(SETTINGS.interval);
-    }, delay * 1000);
+    }, delay);
 }
 
 /**
- * Start a garbage collection engine that runs every <interval> seconds.
+ * Start a garbage collection engine that runs every <interval> milliseconds.
  *
  * For now, it only removes stale user objects based on filter settings set in the environment.
  * Valid filters are:
  *      * GC_USER_LAST_UPDATE - Last update x days ago
  *      * GC_USER_DONE - Whether a validation code should exist
  *
- * @param {*} interval Number of seconds that must elapse before the GC engine runs.
+ * @param {number} interval Milliseconds that must elapse before the GC engine runs.
  */
 function start(interval) {
     // make sure engine is not running
     stop();
 
-    SETTINGS.interval = interval * 1000;
+    SETTINGS.interval = interval;
     RUNNER = setInterval( () => {
         logger.debug('GC: Fired, executing tasks now');
         for (const task of TASKS) {
@@ -68,9 +96,21 @@ function stop() {
     clearInterval(RUNNER);
 }
 
+/**
+ * Reset the GC engine, stopping it and clearing its task list.
+ */
+function reset() {
+    stop();
+
+    // delete everything in the array and destroy references
+    TASKS.length = 0;
+}
+
 module.exports = {
     addTask,
+    removeTask,
     pause,
     start,
-    stop
+    stop,
+    reset
 }
