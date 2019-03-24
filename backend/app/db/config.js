db4924178964a981d2dff877da58a5d445bf51f4
Christopher N. Hesse <raymanfx@gmail.com>
2019-03-24 22:20:59 +0100

    server: db: Never time out initial connection
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- backend/app/db/config.js
+++ backend/app/db/config.js
@@ -6,7 +6,7 @@ module.exports = {
         useNewUrlParser: true,
         poolSize: 10, // Maintain up to 10 socket connections
         bufferMaxEntries: 0, // Return errors immediately rather than waiting for reconnect
-        connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+        connectTimeoutMS: 0, // Never give up initial connection
         socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
         family: 4 // Use IPv4, skip trying IPv6
     }
